bfcee7832b0ab262b53978ce7a079c7a61fa1a3e
Conor Dooley <mail@conchuod.ie>
2022-04-30 19:53:44 +0100

    conf: add "bloated" config
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

---
+++ conf/mainline-bloated/board.mk
@@ -0,0 +1,7 @@
+HSS_SUPPORT ?= y
+HSS_TARGET ?= mpfs-icicle-kit-es
+UBOOT_VERSION = 2022.01
+DEVKIT = polarberry
+linux_defconfig := defconfig
+linux_dtb := $(riscv_dtbdir)/microchip/mpfs-polarberry.dtb
+its_file := $(confdir)/64-bit-fit-image.its
