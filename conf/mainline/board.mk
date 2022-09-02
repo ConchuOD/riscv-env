55da805487cbf0013d3517d1c331c93542d47a75
Conor Dooley <conor.dooley@microchip.com>
2022-09-02 13:54:21 +0100

    mainline: delete specific defconfig
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- conf/mainline/board.mk
+++
@@ -1,7 +0,0 @@
-HSS_SUPPORT ?= y
-HSS_TARGET ?= mpfs-icicle-kit-es
-UBOOT_VERSION = 2022.01
-DEVKIT = icicle-kit-es
-linux_defconfig := defconfig
-linux_dtb := $(riscv_dtbdir)/microchip/microchip-mpfs-icicle-kit.dtb
-its_file := $(confdir)/64-bit-fit-image.its
