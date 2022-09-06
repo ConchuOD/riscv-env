0668abb76e87367ef535412f0afca9f4b2fa925e
Conor Dooley <conor.dooley@microchip.com>
2022-09-06 13:04:53 +0100

    aldec: minimal working config
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- conf/aldec/board.mk
+++ conf/aldec/board.mk
@@ -1,7 +1,8 @@
 HSS_SUPPORT ?= y
 HSS_TARGET ?= aries-m100pfsevp
 UBOOT_VERSION = 2021.04
-linux_defconfig := defconfig
+linux_defconfig := $(confdir)/$(DEVKIT)/defconfig
 linux_dtb := $(riscv_dtbdir)/microchip/mpfs-tysom-m.dtb
+buildroot_initramfs_config := $(confdir)/$(DEVKIT)/buildroot_initramfs_config
 its_file := $(confdir)/32-bit-fit-image.its
 tftp_boot_scr ?= 32b-boot.scr
