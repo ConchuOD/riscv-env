f4657bbd450ea1cfa180234292eb7acc98f1aed4
Conor Dooley <conor.dooley@microchip.com>
2022-09-16 14:12:26 +0100

    icicle-kit-s: rework handling of himem
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- conf/icicle-kit-es/board.mk
+++ conf/icicle-kit-es/board.mk
@@ -1,7 +1,8 @@
 HSS_SUPPORT ?= y
 HSS_TARGET ?= mpfs-icicle-kit-es
 UBOOT_VERSION = 2022.01
-linux_defconfig := mpfs_defconfig
-buildroot_initramfs_config := $(confdir)/buildroot_initramfs_config
+linux_defconfig := defconfig
 linux_dtb := $(riscv_dtbdir)/microchip/mpfs-icicle-kit.dtb
+buildroot_initramfs_config := $(confdir)/buildroot_initramfs_config
 its_file := $(confdir)/64-bit-fit-image.its
+tftp_boot_scr ?= lowmem-boot.scr
