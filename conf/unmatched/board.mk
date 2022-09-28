a79131e289120352fdba1dd697221359cc996f2d
Conor Dooley <mail@conchuod.ie>
2022-09-28 23:05:47 +0100

    unmatched: woo!
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

---
+++ conf/unmatched/board.mk
@@ -0,0 +1,6 @@
+UBOOT_VERSION = 2022.01
+linux_defconfig := $(confdir)/$(DEVKIT)/defconfig
+linux_dtb := $(riscv_dtbdir)/sifive/hifive-unmatched-a00.dtb
+buildroot_initramfs_config := $(confdir)/$(DEVKIT)/buildroot_initramfs_config
+its_file := $(confdir)/32-bit-fit-image.its
+tftp_boot_scr ?= unmatched-boot.scr
