bae4c22068dcc3dff5dc7e5464eb9d26f2425370
Conor Dooley <conor.dooley@microchip.com>
2022-11-15 11:59:26 +0000

    conf: small config updates
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- conf/icicle-kit-es/board.mk
+++ conf/icicle-kit-es/board.mk
@@ -1,7 +1,7 @@
 HSS_SUPPORT ?= y
 HSS_TARGET ?= mpfs-icicle-kit-es
 UBOOT_VERSION = 2022.01
-linux_defconfig := defconfig
+linux_defconfig := $(confdir)/defconfig
 linux_dtb := $(riscv_dtbdir)/microchip/mpfs-icicle-kit.dtb
 buildroot_initramfs_config := $(confdir)/buildroot_initramfs_config
 its_file := $(confdir)/64-bit-fit-image.its
