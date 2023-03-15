cc50f8d544b9f3c1b244d276d8a32cf8bfd3ef16
Conor Dooley <mail@conchuod.ie>
2023-03-15 20:58:02 +0000

    build.py: format with black
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- build.py
+++ build.py
@@ -5,8 +5,8 @@ import re
 import subprocess
 import yaml
 
-class Build:
 
+class Build:
     def __init__(self, config):
         self.cc = try_default(config, "cc", "ccache clang")
         self.cc_path = try_default(config, "cc_path", "")
@@ -23,20 +23,20 @@ class Build:
 
     def simple_build(self):
         command = [
-                    "make",
-                    f"LINUX_CC=\"CC={self.cc}\"",
-                    f"GCC_VERSION={self.gcc_version}",
-                    f"LLVM_VERSION={self.llvm_version}",
-                    f"LINUX_IAS=\"LLVM_IAS={self.llvm_ias}\"",
-                    f"CLANG={self.llvm}",
-                    f"{self.type}",
-                    f"DEVKIT={self.devkit}"
-                  ]
+            "make",
+            f'LINUX_CC="CC={self.cc}"',
+            f"GCC_VERSION={self.gcc_version}",
+            f"LLVM_VERSION={self.llvm_version}",
+            f'LINUX_IAS="LLVM_IAS={self.llvm_ias}"',
+            f"CLANG={self.llvm}",
+            f"{self.type}",
+            f"DEVKIT={self.devkit}",
+        ]
 
         if self.ld_path != None:
-            command.append(f"LINUX_LD=\"LD={self.ld_path}\"")
+            command.append(f'LINUX_LD="LD={self.ld_path}"')
         if self.as_path != None:
-            command.append(f"LINUX_AS=\"AS={self.as_path}\"")
+            command.append(f'LINUX_AS="AS={self.as_path}"')
 
         stdout = []
 
@@ -48,12 +48,11 @@ class Build:
             command,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
-            universal_newlines = True
+            universal_newlines=True,
         )
 
         # spit out stdout so the program seems alive
         while True:
-
             if process.stdout != None:
                 output = process.stdout.readline()
                 stdout.append(output)
@@ -74,25 +73,30 @@ class Build:
     def dry_run(self):
         self.dry_run = True
 
+
 def try_default(config, key, default):
     try:
         return config[key]
     except KeyError:
         return default
 
-if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description = 'kbuild helper')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="kbuild helper")
 
-    parser.add_argument('-c', '--config', help = 'yaml config file', default = 'conf/targets.yaml')
-    parser.add_argument('-t', '--target', help = 'the target config', default = 'default')
-    parser.add_argument('--dry-run', help = "dry run", default = False, action = 'store_true')
-    parser.add_argument('--pattern', help = "pattern to match in stderr")
-    parser.add_argument('--linux-dir', help = "directory to build linux from", default = "/stuff/linux")
+    parser.add_argument(
+        "-c", "--config", help="yaml config file", default="conf/targets.yaml"
+    )
+    parser.add_argument("-t", "--target", help="the target config", default="default")
+    parser.add_argument("--dry-run", help="dry run", default=False, action="store_true")
+    parser.add_argument("--pattern", help="pattern to match in stderr")
+    parser.add_argument(
+        "--linux-dir", help="directory to build linux from", default="/stuff/linux"
+    )
 
     args = parser.parse_args()
 
-    with open(args.config, 'r') as stream:
+    with open(args.config, "r") as stream:
         try:
             config = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
@@ -116,4 +120,3 @@ if __name__ == '__main__':
                 matches = matches + 1
                 print(line.strip())
         print(f"{matches} matches in {lines} lines")
-
