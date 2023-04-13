008a92c9dfe1ae0becc4907ef147f15fb78e3bef
Conor Dooley <mail@conchuod.ie>
2023-04-13 22:35:09 +0100

    conf: delete mainline-bloated
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- conf/mainline-bloated/board.mk
+++
@@ -1,8 +0,0 @@
-HSS_SUPPORT ?= y
-HSS_TARGET ?= mpfs-icicle-kit-es
-UBOOT_VERSION = 2022.01
-DEVKIT = polarberry
-linux_defconfig := defconfig
-buildroot_initramfs_config := $(confdir)/$(DEVKIT)/buildroot_initramfs_config
-linux_dtb := $(riscv_dtbdir)/microchip/mpfs-polarberry.dtb
-its_file := $(confdir)/32-bit-fit-image.its
