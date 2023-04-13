eafd6ae4a9ca3216c3ec2b074c0d53327456e9d9
Conor Dooley <mail@conchuod.ie>
2023-04-13 22:34:26 +0100

    conf: delete useless crap
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- conf/overlay/board.mk
+++ conf/overlay/board.mk
@@ -1,6 +1,7 @@
 HSS_SUPPORT ?= y
 HSS_TARGET ?= mpfs-icicle-kit-es
-linux_defconfig := $(confdir)/$(DEVKIT)/defconfig
+linux_defconfig := $(confdir)/defconfig
+linux_fragment := $(confdir)/$(DEVKIT)/515_nc.config
 linux_dtb := $(riscv_dtbdir)/microchip/mpfs-icicle-kit.dtb
 buildroot_initramfs_config := $(confdir)/$(DEVKIT)/buildroot_initramfs_config
 its_file := $(confdir)/38-bit-fit-image.its
