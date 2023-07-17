959f4108b42e89d4027352164d8c8d9cc102a82e
Conor Dooley <conor.dooley@microchip.com>
2023-07-17 15:03:45 +0100

    polarberry: I have one again, make it bootable once more
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- conf/polarberry/board.mk
+++ conf/polarberry/board.mk
@@ -1,7 +1,7 @@
 HSS_SUPPORT ?= y
 HSS_TARGET ?= polarberry
 UBOOT_VERSION = 2021.04
-linux_defconfig := defconfig
+linux_defconfig := $(confdir)/defconfig
 linux_dtb := $(riscv_dtbdir)/microchip/mpfs-polarberry.dtb
 buildroot_initramfs_config := $(confdir)/$(DEVKIT)/buildroot_initramfs_config
 its_file := $(confdir)/32-bit-fit-image.its
