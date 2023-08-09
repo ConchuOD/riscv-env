0a68cb4e74c323e4efe31565a56b717fcf5e6e25
Conor Dooley <mail@conchuod.ie>
2023-08-09 23:24:18 +0100

    assorted conf changes :)
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- conf/ahead/board.mk
+++ conf/ahead/board.mk
@@ -1,6 +1,7 @@
+OSBI_SUPPORT ?= y
 UBOOT_VERSION = 2021.04
 linux_defconfig := $(confdir)/defconfig
 linux_dtb := $(riscv_dtbdir)/thead/th1520-lichee-pi-4a.dtb
 its_file := $(confdir)/$(DEVKIT)/fit-image.its
-buildroot_initramfs_config := $(confdir)/buildroot_initramfs_config
+buildroot_initramfs_config := $(confdir)/$(DEVKIT)/buildroot_initramfs_config
 tftp_boot_scr := ahead-boot.scr
